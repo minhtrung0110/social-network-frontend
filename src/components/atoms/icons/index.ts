992646e647878f5dd0969fff6396cd8fda012d45
minhtrung0110 <nguyenducminhtrung0044@gmail.com>
2024-04-09 19:26:01 +0700

    feat(app/components): add some icons and  components

--- src/components/atoms/icons/index.ts
+++ src/components/atoms/icons/index.ts
@@ -3,3 +3,5 @@ export { EditIcon } from './EditIcon';
 export { DeleteIcon } from './DeleteIcon';
 export { EmotionIcon } from './EmotionIcon';
 export { GoogleIcon } from './GoogleIcon';
+export { GithubIcon } from './GithubIcon';
+export { SpinnerIcon } from './SpinnerIcon';
