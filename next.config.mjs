cbb4efab23297743a3528315f6480aff3f9f910d
minhtrung0110 <nguyenducminhtrung0044@gmail.com>
2024-04-25 01:29:38 +0700

    feat(next-config): config project
    add hostname image
    add some util functions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        // port: '4000',
+        // // pathname: '/photos/**'
+      },
+    ],
+  },
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+};
 
 export default nextConfig;
