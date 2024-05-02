913f13471bb63159b925491c369735507b5ddc5a
minhtrung0110 <nguyenducminhtrung0044@gmail.com>
2024-05-03 02:30:14 +0700

    refactor(tailwindcss and icon): refactor style tailwindcss and add some icons

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,7 @@ const config = {
     extend: {
       colors: {
         border: 'hsl(var(--border))',
+        gradient: 'var(--gradient)',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
         background: 'hsl(var(--background))',
